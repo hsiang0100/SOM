fe2cd6c1177ffaf1e60badfb29d416fbb31167c0
hsiang <myluck0482@gmail.com>
2016-12-26 13:17:27 +0800

    edit SOM gain term

--- main.m
+++ main.m
@@ -1,6 +1,7 @@
 clc
 clear all
 close all
+
 for i=1:1000
     x1(i)=rand*10;
     x2(i)=rand*10;
@@ -18,10 +19,24 @@ plot(w1,w2,'or');
 plot(w1,w2,'k','linewidth',2);
 plot(w1',w2','k','linewidth',2);
 hold off;
+%=======Initialize parameters=======
+T=300;
+MapRadius=5;
 
-for i=1:1000
-    d=(x1(i)-w1).^2+(x2(i)-w2).^2;
-    min=min(min(d));
-    [row,col] = find(d==min);
+for t=0:300
+    eta=(1-t/T);
+
+    for i=1:1000
+        d=(x1(i)-w1).^2+(x2(i)-w2).^2;
+        min_d=min(min(d));
+        [row,col] = find(d==min_d);
+        %winner
+        w1(row,col)=w1(row,col)+eta*(x1(i)-w1(row,col));
+        w2(row,col)=w2(row,col)+eta*(x2(i)-w2(row,col));
+        %neighbor
+        TimeConstant = T/log(MapRadius);
+        NeighbourhRadius = m_dMapRadius * exp(-(double)m_iIterationCount/m_dTimeConstant);
+    end
+    
 end
 
